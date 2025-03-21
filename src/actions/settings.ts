3ee653f63f3629c57de58b917776c328dda133a2
montaser <montaserreq@gmail.com>
2025-03-21 14:59:19 +0300

    fix bug2 - deploy

--- src/actions/settings.ts
+++ src/actions/settings.ts
@@ -9,17 +9,13 @@ import bcryptjs from "bcryptjs";
 import { currentUser } from "@/lib/user";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
-  const user = await currentUser();
+  console.log("Values received in settings:", values); // لمراقبة البيانات المدخلة
 
-  if (!user || !user.id) {
-    return { error: "Unauthorized" };
-  }
+  const user = await currentUser();
+  if (!user || !user.id) return { error: "Unauthorized" };
 
   const dbUser = await getUserById(user.id);
-
-  if (!dbUser) {
-    return { error: "Unauthorized" };
-  }
+  if (!dbUser) return { error: "Unauthorized" };
 
   if (user.isOAuth) {
     values.email = undefined;
@@ -28,7 +24,7 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     values.isTwoFactorEnabled = undefined;
   }
 
-  if (values.email && values.email !== user.email) {
+  if (values.email && typeof values.email === "string" && values.email !== user.email) {
     const ExistingUser = await getUserByEmail(values.email);
 
     if (ExistingUser && ExistingUser.id !== user.id) {
@@ -42,26 +38,20 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   }
 
   if (values.newPassword && values.password && dbUser.password) {
-    const passwordsMatch = await bcryptjs.compare(
-      values.password,
-      dbUser.password
-    );
+    const passwordsMatch = await bcryptjs.compare(values.password, dbUser.password);
 
     if (!passwordsMatch) {
       return { error: "Invalid password" };
     }
 
     const hashedPassword = await bcryptjs.hash(values.newPassword, 10);
-
     values.password = hashedPassword;
     values.newPassword = undefined;
   }
 
   await db.user.update({
     where: { id: dbUser.id },
-    data: {
-      ...values,
-    },
+    data: { ...values },
   });
 
   return { success: "Settings Updated!" };
